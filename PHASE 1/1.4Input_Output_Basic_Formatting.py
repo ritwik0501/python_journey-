01df40c107422a67b080fa7f20ac804a60250dd8
Ritwik <gritwik5843@gmail.com>
2026-09-06 18:14:21 +0530

    added input output basic formatting

--- PHASE 1/1.4Input_Output_Basic_Formatting.py
+++ PHASE 1/1.4Input_Output_Basic_Formatting.py
@@ -8,8 +8,31 @@
 #twelve like this instade of 12 it will surely crash the code 
 #then to tackle this we use try expect block 
 
-try:
-    print("Enter number")
-    num1=int(input())
-except Exception as e:
-    print("Error",e)
+# try:
+#     print("Enter number")
+#     num1=int(input())
+# except Exception as e:
+#     print("Error",e)
+
+#this code is use to show loading but not progress bar 
+# import time
+
+# for i in range(1,100,2):
+#     print(f"Loading in {i}...",end="\r")
+#     time.sleep(1)
+
+# print("Done!")
+
+# task 1 Build a progress bar that prints on one line using `end="\r"`.
+
+import time 
+
+for i in range(101):
+    bar= "#" * (i//5)
+    spaces=" "*(20-len(bar))
+    
+    print(f"Downloading...[{bar}{spaces}]",end="\r")
+    # time.sleep(0.1)
+    
+print("Successfully Download!`              ")
+    
