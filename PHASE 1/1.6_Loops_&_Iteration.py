98f78ab56314c74dc7bcbbee16815a9e376da0a9
Ritwik <gritwik5843@gmail.com>
2026-09-22 04:39:26 +0530

    added another drills

--- PHASE 1/1.6_Loops_&_Iteration.py
+++ PHASE 1/1.6_Loops_&_Iteration.py
@@ -28,17 +28,25 @@
 
 # diamond
  
-for i in range(1,5):
-    for j in range(5-i):
-        print(" ",end="")
-    for j in range(2*i-1):
-        print("*",end="")
-    print()
+# for i in range(1,5):
+#     for j in range(5-i):
+#         print(" ",end="")
+#     for j in range(2*i-1):
+#         print("*",end="")
+#     print()
 
-for j in range(5,0,-1):
-    for i in range(5-j):
-        print(" ",end="")
-    for i in range(2*j-1):
-        print("*",end="")
-    print()
+# for j in range(5,0,-1):
+#     for i in range(5-j):
+#         print(" ",end="")
+#     for i in range(2*j-1):
+#         print("*",end="")
+#     print()
  
+# - [ ] Sum every number from 1 to 100 divisible by 3 or 5.
+
+sum=0
+for i in range(1,101):
+    if i%3==0 or i%5==0:
+        sum=sum+i
+        
+print("Sum of every number from 1 to 100 divisible by 3 or 5",sum)
